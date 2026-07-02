667cdce6669d28bcc4e74356d83691a740da6c74
Viktor <a0000@MacBook-Air-0000.local>
2026-07-02 21:47:23 +0700

    +swagger + json serialization

--- DissCouncil.App/Program.cs
+++ DissCouncil.App/Program.cs
@@ -7,11 +7,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 //добавление сервисов (что приложение умеет делать - регистрирование способностей)
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters
+            .Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IDissertationRepository, DissertationRepository>();
 builder.Services.AddScoped<IDissertationService, DissertationService>();
+
+//сбор инфы про эндпоинты
+builder.Services.AddEndpointsApiExplorer();
+// генерация описания по этой инфе
+builder.Services.AddSwaggerGen();
 
 // граница (собираем все регистрации в готовое приложение)
 var app = builder.Build();  // готовое приложение из всех регистраций
@@ -19,6 +27,8 @@ var app = builder.Build();  // готовое приложение из всех
 // middleware-конвейер (она же цепочка обработчиков запроса)
 app.UseHttpsRedirection();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // запуск приложения
 app.Run();
