34657c4f5d9bd5a5950cfaffc575b16894773cf6
Viktor <a0000@MacBook-Air-0000.local>
2026-07-12 20:47:22 +0700

    completed dtos for the applicant, added applicant service + service interface

---
+++ DissCouncil.App/DTOs/CreateApplicantDto.cs
@@ -0,0 +1,10 @@
+using DissCouncil.Domain.Enums;
+
+namespace DissCouncil.App.DTOs;
+
+public class CreateApplicantDto
+{
+    public required string FullName { get; set; }
+    public required string Organization { get; set; }
+    public AcademicDegree Degree { get; set; }
+}
